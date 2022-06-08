37c50fee3d7bc665a21d346286e6e54503c20db8
Ian <53247327+Birb12@users.noreply.github.com>
2022-06-07 17:10:12 -0700

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,2 +1,21 @@
+mov ah, 0x0e
+mov bx, welcomefoog
+
   
+printfoog:
+    mov al, [bx + 0x7c00]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp printfoog
+end:
+
+jmp $
+
+welcomefoog:
+    db "Welcome to Foog OS. Hail Lord Foog the 2st", 0
+times 510-($-$$) db 0
+dw 0xaa55
+
  
